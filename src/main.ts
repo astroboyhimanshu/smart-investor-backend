f22a6bed1858311d852c9356be6470f0e6537384
Astroboy Himanshu <hhimanshu_be20@thapar.edu>
2025-06-26 23:12:50 +0530

    fix: allow vercel frontend in CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   dotenv.config(); // load .env
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' }); // allow frontend calls
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://smart-investor-sand.vercel.app'],
+  }); // allow frontend calls
   await app.listen(3000);
 }
 bootstrap();
